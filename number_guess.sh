3144272ed6cb20b08aa13c0dce9fbbdbb84194dc
FREDY AKTOSA <workmailAkto@gmail.com>
2024-04-16 05:10:54 +0000

    feat: Adding input for username and number the user send

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,50 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 ANSWER_NUMBER_RANDOM=$((RANDOM % 1000 + 1))
 
+echo -e "\n~~~ Number Guessing Game ~~~\n"
+
+echo "Enter your username:"
+read USERNAME
+
+DATA_USER=$($PSQL "SELECT * FROM users_data WHERE username = '$USERNAME'")
+if[[ -z $DATA_USER ]]
+then
+  INSERTED_DATA_USER=$($PSQL "INSERT INTO users_data(username, games_played, best_game) VALUES ($USERNAME, 0, 0)")
+  if [[ $INSERTED_DATA_USER = "INSERT 0 1" ]]
+  then
+      echo -e "\nWelcome, $USERNAME! It looks like this is your first time here.\n"
+  fi
+else
+  GAME_PLAYED=$(echo "$DATA_USER" | cut -d '|' -f 3)
+  BEST_GAME=$(echo "$DATA_USER" | cut -d '|' -f 4)
+  echo -e "\nWelcome back, $USERNAME!, You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses.\n"
+fi
+
+echo -e "Guess the secret number between 1 and 1000:"
+read USER_GUESS_NUMBER
+NUMBER_OF_GAMES=0
+
+if [[ $USER_GUESS_NUMBER != $ANSWER_NUMBER_RANDOM ]]
+then
+  while [[ $USER_GUESS_NUMBER != $ANSWER_NUMBER_RANDOM ]]
+  do
+    NUMBER_OF_GAMES+=1
+    if [[ $USER_GUESS_NUMBER > $ANSWER_NUMBER_RANDOM ]]
+    then
+      echo -e "\nIt's lower than that, guess again:"
+      read USER_GUESS_NUMBER
+    elif [[ $USER_GUESS_NUMBER < $ANSWER_NUMBER_RANDOM ]]
+    then
+      echo -e "\nIt's higher than that, guess again:"
+      read USER_GUESS_NUMBER
+    elif [[ ! $USER_GUESS_NUMBER =~ ^[0-9]+$ ]]
+    then
+      echo -e "\nThat is not an integer, guess again:"
+      read USER_GUESS_NUMBER
+    else
+      echo -e "\nYou guessed it in $NUMBER_OF_GAMES tries. The secret number was $ANSWER_NUMBER_RANDOM. Nice job!\n"
+    fi
+  done
+else
+  echo -e "\nYou guessed it in 1 tries. The secret number was $ANSWER_NUMBER_RANDOM. Nice job!\n"
+fi
